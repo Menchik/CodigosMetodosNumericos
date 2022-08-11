8c340196c56f986108d78bb9a46ff3ea8d553ba8
Allan <allan.menchik@hotmail.com>
2022-08-11 07:57:33 -0400

    Newton feito

--- newton.py
+++ newton.py
@@ -1,20 +1,17 @@
-import math as m
 import sympy as sp
 
-def newton(a, erro, maxit):
+def newton(f, a, erro, maxit):
     k = 1
-    x = sp.symbols("x")
-    f = x**2-sp.exp(-x)
 
     while k <= maxit:
         
         fa = f.subs(x, a)
         df = sp.diff(f, x)
-        c = a - fa/df.subs(x, a)
-        print(c)
+        da = df.subs(x, a)
+        c = a - fa/da
 
         if abs(c-a) < erro or abs(c-a)/abs(c) < erro or abs(f.subs(x, c)) < erro:
-            print(f"valor: {c}\niterações: {k}")
+            print(f"valor: {c.evalf()}\niterações: {k}")
             return
         
         a = c
@@ -23,7 +20,9 @@ def newton(a, erro, maxit):
     print("Dumbass")
 
 if __name__ == "__main__":
+    x = sp.Symbol("x")
+    f = x**2-sp.exp(-x)
     a = 0
     erro = 0.01
     maxit = 10
-    newton(a, erro, maxit)
+    newton(f, a, erro, maxit)
